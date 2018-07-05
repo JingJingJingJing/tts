06707362b5f984728ef5bae46bd3b7d26523ea4e
Jing <summerrymaker@gmail.com>
2018-07-05 23:56:40 +0800

    complete the basic function of replace <a> attr

--- src/synctags.py
+++ src/synctags.py
@@ -1,16 +1,27 @@
 import json
-import sys
-import io
+from collections import OrderedDict
+import re
    
-def synca(node):
+def synca(node, target):
     for key in node:
         if isinstance(node[key],dict):
-            sync(node[key])
+            if key in target:
+                synca(node[key],target[key])
         else:
-            if "<a" in node[key]:
-                print(node[key])
-                print(" ")
-        
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
-en = open("src//test.json", encoding='utf-8')
-synca(json.load(en))
+            if "<a" in node[key] and key in target:
+                olds = geta(node[key])
+                news = geta(target[key])
+                if len(olds) == len(news):
+                    for i,old in enumerate(olds):
+                        node[key] = node[key].replace(old,news[i],1)
+
+def geta(s):
+    pattern = re.compile("(<a.*?>).*?</a>")
+    return re.findall(pattern,s)
+
+node = json.load(open("src//test.json"),object_pairs_hook=OrderedDict)
+target = json.load(open("src//target.json"),object_pairs_hook=OrderedDict)
+synca(node, target)
+out = open("t.json","w")
+out.write(json.dumps(node, indent=4))
+out.close()
