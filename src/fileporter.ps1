f3f10321b9a21f964a777f0c05f960678f62234c
Gian Zheng <zhengxp3@lenovo.com>
2019-05-31 13:28:33 +0800

    update file portor to adapt random file name

--- src/fileporter.ps1
+++ src/fileporter.ps1
@@ -2,7 +2,7 @@ $hash = @{"AR"="ar";"BR"="pt-BR";"CR"="hr";"CZ"="cs";"DK"="da";
 			"FI"="fi";"FR"="fr";"GK"="el";"GR"="de";"HE"="he";
 			"HU"="hu";"IT"="it";"JP"="ja";"KR"="ko";"NL"="nl";
 			"NO"="nb";"PL"="pl";"PO"="pt";"RO"="ro";"RU"="ru";
-			"SC"="zh-Hans";"SH"="sr-Latn";"SK"="sk";"SL"="sl";
+			"SC"="zh-Hans";"SH"="sr";"SK"="sk";"SL"="sl";
 			"SP"="es";"SV"="sv";"TC"="zh-Hant";"TU"="tr";"UA"="uk"}
 Get-ChildItem ./ | ForEach-Object -Process{
 	if($_-is [System.IO.DirectoryInfo]){
@@ -10,18 +10,11 @@ Get-ChildItem ./ | ForEach-Object -Process{
 			if(-not (Test-path json)){
 				mkdir json
 			}
-			$old_path = $_.name+"/en.json"
-			if(-not (Test-path $old_path)){
-				$old_path = $_.name+"/"+$hash[$_.name]+".json"
-			}
-			if(-not (Test-path $old_path)){
-				$old_path = $_.name+"/"+$_.name+".json"
-			}
-			if(Test-path $old_path){
-				$new_path = "json/"+$hash[$_.name]+".json"
-				Move-Item $old_path $new_path
-				Remove-Item $_.name
-			}
+			$path = $_.name+"/"
+			$contents = $path + (Get-ChildItem -Path $path -Force -Recurse -File | Select-Object -First 1)
+			$new_path = "json/"+$hash[$_.name]+".json"
+			Move-Item $contents $new_path
+			Remove-Item $_.name
 		}
 	}
 }
